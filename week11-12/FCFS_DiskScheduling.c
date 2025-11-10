74448dc33b4ab3be1ac0e976dc42545fe5b35ce7
RohitGaira <rohitgaira96@gmail.com>
2025-11-10 03:20:05 +0000

    week11-12

---
+++ week11-12/FCFS_DiskScheduling.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n, head, seek_time=0, curr;
+    printf("Enter the number of requests: ");
+    scanf("%d", &n);
+    int requests[n];
+    printf("Enter the requests:\n");
+    for(int i=0; i<n; i++){
+        scanf("%d", &requests[i]);
+    }
+    head=requests[0];
+    for(int i=0; i<n; i++){
+        curr = requests[i];
+        seek_time += (curr > head) ? (curr - head) : (head - curr);
+        head = curr;
+    }
+    printf("Total Seek Time: %d\n", seek_time);
+    return 0;
+}
